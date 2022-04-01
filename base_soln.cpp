f022d44aeb1bec8ebbe0de0ed9655d1a158f150a
Ankush1369 <ankush2001garg@gmail.com>
2022-04-02 00:14:22 +0530

    Adding plt_to_csv functionality, distance can be choosen b/w euclidean or spherical, adding loose lowerbound for DFD

--- base_soln.cpp
+++ base_soln.cpp
@@ -9,19 +9,19 @@
 #define int long long
 using namespace std;
 
-int n;                     // k being then minimim motif length
+int n;
 vector<pair<float, float> > S; // input Trajectory
 vector<vector<float> > dfd;
 float R = 6371.00;
 
 bool valid(int a, int b, int k)
 {
-    return abs(b-a + 1) >= k;
+    return abs(b - a + 1) >= k;
 }
 
-// Assuming cartersian coordinates for now
+// Assuming grid coordinates as in dataset
 // Can be easily modified for any other form of coordinates
-float distance(int i, int j)
+float spherical_distance(int i, int j)
 {
     float d = 2 * R * asin(sqrt(sin((S[j].first - S[i].first) / 2) * sin((S[j].first - S[i].first) / 2) + cos(S[i].first) * cos(S[j].first) * sin((S[j].second - S[i].second) / 2) * sin((S[j].second - S[i].second) / 2)));
     return d;
@@ -29,9 +29,16 @@ float distance(int i, int j)
 
 float euclidean_distance(int i, int j)
 {
-    float d = sqrt(pow(S[i].first-S[j].first, 2) + pow(S[i].second-S[j].second, 2));
+    float d = sqrt(pow(S[i].first - S[j].first, 2) + pow(S[i].second - S[j].second, 2));
     return d;
 }
+
+float distance(int i, int j)
+{
+    return spherical_distance(i, j);
+}
+
+
 
 int read_dataset(string fname)
 {
@@ -74,13 +81,20 @@ int read_dataset(string fname)
 
 vector<pair<int, int> > motif(int k)
 {
-    int bestDFD = INT64_MAX;
+    float bestDFD = 100000000;
     vector<pair<int, int> > v(2);
+   // cout << n << '\n';
     for (int startFirst = 0; startFirst < n && (startFirst + 2 * k - 1) < n; startFirst++)
-    {
+    {   
         for (int startSecond = startFirst + k; startSecond < n; startSecond++)
         {
             dfd[startFirst][startSecond] = distance(startFirst, startSecond);
+            if(dfd[startFirst][startSecond]>bestDFD){
+                continue;
+                //loose lower bound on DFD which is the distance between starting points
+            }
+            
+
 
             for (int tmp = startSecond + 1; tmp < n; tmp++)
             {
@@ -116,20 +130,43 @@ vector<pair<int, int> > motif(int k)
     return v;
 }
 
+void plt_to_csv(string filename, string outfile)
+{
+    string line;
+    ifstream b_file(filename);
+    ofstream output_file;
+    output_file.open(outfile);
+    int i = 0;
+    while (getline(b_file, line))
+    {
+        if (i > 6)
+        {
+            output_file << line << endl;
+        }
+        i++;
+    }
+}
+
 int32_t main()
 {
     string filename = "dataset.csv";
+    // vector<float> xc = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    // vector<float> yc = {5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, -5, -4, -3, -2, -1, 0, 0, 0, 0, 0, 0, 0};
+    //sample testcase 
+    cout << "Enter plt filename for Geolife dataset: ";
+    string pltname;
+    cin >> pltname;
+    plt_to_csv(pltname, filename);
     n = read_dataset(filename);
+
     dfd.resize(n, vector<float>(n, 100000000)); // initialzing dfd as max
-    int m;
-    int motif_length;
-    cin >> m >> motif_length;
-    n = min(m, n);
-    motif_length = min(motif_length, n/2);
-    // cout << n << '\n';
-    // for(auto i: S){
-    //     cout << i.first << " " << i.second << '\n';
-    // }
+    int m, motif_length;
+    cout << "Enter tarjectory length to be analyized: ";
+    cin >> m;
+    cout << "Enter motif_length to be considered: ";
+    cin >> motif_length;
+    n = min(n, m);
+    motif_length = min(n/2, motif_length);
     vector<pair<int, int> > res = motif(motif_length);
     for(auto i: res){
         cout << i.first << " "  << i.second << '\n';
