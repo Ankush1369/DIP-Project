d543090231259d1036c0c72f83dc11431c3ed6b7
ManikGarg316 <navigarg779@gmail.com>
2022-04-04 11:26:11 +0530

    piecewise linear function-incomplete

--- piecewise_linear.cpp
+++ piecewise_linear.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
+
+//-1 t be replaced by infinity
 int E[1000][1000];
+int F[1000][1000];
+int I[1000][1000];
+int A[1000][1000];
+int X[1000][1000];
+int B[1000][1000];
+int n;
+int T = n/7;
 int piecewise(int points[][2])
 {
  for(int i=0;i<n;i++)
@@ -27,8 +36,8 @@ int piecewise(int points[][2])
    }
    m_i_j = ((j-i+1)*xy - x*y)/((j-i+1)*x_2 - x*x);
    c_i_j = (y - m_i_j*x)/(j-i+1);
-   //int M_i_j = m_i_j;
-   //int C_i_j = c_i_j;
+   A[i][j] = m_i_j;
+   B[i][j] = c_i_j;
    E[i][j] = 0;
    for(int k=i;k<=j;k++)
    {
@@ -36,5 +45,23 @@ int piecewise(int points[][2])
    }
   }
  }
-
+ for(int j=0;j<n;j++)
+ {
+  for(int t=j;t<=T;t++)
+  {
+   F[j][t] = -1;
+   I[j][t] = 0;
+   X[j][t] = 0;
+  }
+  F[j][0] = E[0][j];
+  I[j][0] = 1;
+  X[j][1] = 0;
+ }
+ for(int t=1;t<=min(j-1,T);t++)
+ {
+  F[j][t] = -1;
+  I[j][t] = 0;
+  X[j][0] = 0;
+  for(int i=t;t<=j-1)
+ }
 }
