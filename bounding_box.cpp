937440e35e731080beff9272ff349ee4c5f2a2b8
Ankush1369 <ankush2001garg@gmail.com>
2022-04-29 14:11:56 +0530

    Adding code for Bounding Box with adaptive percentage and normal breakage condition and also updating some points in Plot_1(csv)

--- bounding_box.cpp
+++ bounding_box.cpp
@@ -1,20 +1,28 @@
-#include<iostream>
-#include<vector>
-#include<utility>
-#include<cmath>
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#include<float.h>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <time.h>
+#include <float.h>
+#include <sstream>
+#include <algorithm>
 using namespace std;
 
-vector<vector<double>> line_segments; 
+struct boundingBox{
+    int start;
+    int end;
+    pair<double, double> width;
+};
 
-vector<pair<double, double>> points;
+vector<vector<double> > line_segments; 
+
+vector<pair<double, double> > points;
 double abs_put_value(pair<double,double> point, pair<double, double> start, pair<double, double> end)
 {
-    double put_val= round(10000*point.second*(end.first - start.first) - point.first*(end.second - start.second) + start.first*(end.second - start.second) - start.second*(end.first - start.first))/10000;
+    double put_val= point.second*(end.first - start.first) - point.first*(end.second - start.second) + start.first*(end.second - start.second) - start.second*(end.first - start.first);
     if(put_val < 0)
     {
         return -put_val;
@@ -24,17 +32,18 @@ double abs_put_value(pair<double,double> point, pair<double, double> start, pair
 
 double dist_between_points(pair<double, double> start, pair<double, double> end)
 {
-    double ans = round(10000*sqrt((start.first - end.first)*(start.first - end.first) + (start.second - end.second)*(start.second - end.second)))/10000;
+    double ans = sqrt((start.first - end.first)*(start.first - end.first) + (start.second - end.second)*(start.second - end.second));
     return ans;
 }
 
 double dist_from_line(pair<double,double> point, pair<double, double> start, pair<double, double> end)
 {
-    double ans = round(10000*abs_put_value(point, start, end)/dist_between_points(start, end))/10000;
+
+    double ans = abs_put_value(point, start, end)/dist_between_points(start, end);
     return ans;
 }
 
-int put_bin(pair<double, double> point, pair< double, double> start, pair<double, double> end)
+int find_region(pair<double, double> point, pair< double, double> start, pair<double, double> end)
 {
     double put_val= point.second*(end.first - start.first) - point.first*(end.second - start.second) + start.first*(end.second - start.second) - start.second*(end.first - start.first);
     if(put_val < 0)
@@ -44,87 +53,77 @@ int put_bin(pair<double, double> point, pair< double, double> start, pair<double
     return 1;   
 }
 
-int bounding_box_segmentations(vector<pair<double, double>> points)
-{
-    int n = points.size();
-    int start = 0;
-    int end = 2;
-    int curr_length = 0;
-    while(end < n)
-    {
-        double min_bbox_positive = DBL_MAX;
-        double min_bbox_negative = DBL_MAX;
-        double length_min_bbox = 0;
-        int breaking_point = 0;
-        pair<double,double> min_bbox_posicoord(DBL_MAX, DBL_MAX);
-        pair<double,double> min_bbox_negacoord(DBL_MAX, DBL_MAX);
-        for(int i=end;i<n;i++)                                                                           
-        {
-            double max_dist_positive = 0;
-            double max_dist_negative = 0;
-            double length = dist_between_points(points[start], points[i]);
-            pair<double, double> positive_coord(DBL_MAX, DBL_MAX);
-            pair<double, double> negative_coord(DBL_MAX, DBL_MAX);
-            for(int j=start+1;j<end;j++)
-            {
-                //double w = distance of points[j] from line formed between p[start] and p[i]
-                double w = dist_from_line(points[j], points[start], points[i]);
-                cout << w << '\n';
-                if(put_bin(points[j], points[start], points[i]) >= 0 and w>max_dist_positive)
-                {
-                    max_dist_positive = w;
-                    positive_coord = points[j];
-                }
-                if(put_bin(points[j], points[start], points[i]) < 0 and w>max_dist_negative)
-                {
-                    max_dist_negative = w;
-                    negative_coord = points[j];
-                }
-            }
-            if(min_bbox_positive + min_bbox_negative > (max_dist_positive+max_dist_negative)/length)
-            {
-                min_bbox_positive = max_dist_positive/length;
-                min_bbox_negative = max_dist_negative/length;
-                breaking_point = i;
-                min_bbox_posicoord = positive_coord;
-                min_bbox_negacoord = negative_coord;
-                length_min_bbox = length;
-            }
+pair<double, double> find_bounding_box(vector<pair<double, double> > &points, int start, int end){
+    double max_upper = 0;
+    double max_lower = 0;
+    for(int i=start+1; i<end; i++){
+        if(find_region(points[i], points[start], points[end]) > 0){
+            max_upper = max(max_upper, dist_from_line(points[i], points[start], points[end]));
+        }else{
+            max_lower = max(max_lower, dist_from_line(points[i], points[start], points[end]));
         }
-        //line_segments.add((min_bbox_positive, min_bbox_negative, length_min_bbox, breaking_point, min_bbox_positivecoord, min_bbox_negativecoord))
-        vector<double> line_seg;
-        line_segments.push_back(line_seg);
-        line_segments[curr_length].push_back(min_bbox_positive);
-        line_segments[curr_length].push_back(min_bbox_negative);
-        line_segments[curr_length].push_back(length_min_bbox);
-        line_segments[curr_length].push_back(breaking_point);
-        line_segments[curr_length].push_back(min_bbox_posicoord.first);
-        line_segments[curr_length].push_back(min_bbox_posicoord.second);
-        line_segments[curr_length].push_back(min_bbox_negacoord.first);
-        line_segments[curr_length].push_back(min_bbox_negacoord.second);
-        curr_length++;
-        start = breaking_point + 1;
-        end = start + 2;
     }
-    if(end == n)
+    pair<double, double> res(max_upper, max_lower);
+    return res;
+}
+
+
+vector<boundingBox> bounding_box_segmentations(vector<pair<double, double> > &points,int n, double max_percentage = 110.0)
+{
+    if(n>(int)points.size()){
+        n = points.size();
+    }
+    int start = 0;
+    double percentage;
+    max_percentage -= 110.0;
+    if(max_percentage<0){
+        max_percentage = 0;
+    }
+    vector<boundingBox> res;
+    while(start<n)
     {
-        vector<double> line_seg;
-        line_segments.push_back(line_seg);
-        line_segments[curr_length].push_back(DBL_MAX);
-        line_segments[curr_length].push_back(DBL_MAX);
-        line_segments[curr_length].push_back(dist_between_points(points[n-2], points[n-1]));
-        line_segments[curr_length].push_back(n-1);
-        pair<double,double> min_bbox_posicoord(DBL_MAX, DBL_MAX);
-        pair<double,double> min_bbox_negacoord(DBL_MAX, DBL_MAX);
-        line_segments[curr_length].push_back(min_bbox_posicoord.first);
-        line_segments[curr_length].push_back(min_bbox_posicoord.second);
-        line_segments[curr_length].push_back(min_bbox_negacoord.first);
-        line_segments[curr_length].push_back(min_bbox_negacoord.second);
-        curr_length++;
-        end=n;
+        int end = start+2;
+        double difference = max_percentage;
+        double percentage;
+        double minimum_prev_ratio = DBL_MAX;
+        pair<double, double> prev_width(DBL_MAX, DBL_MAX);
+        bool found = false;
+        cout << "Starting new BB at: " << start << '\n';
+        while(end<n){
+            percentage = 110.0 + difference;
+            pair<double, double> width = find_bounding_box(points, start, end);
+            double current_ratio = (width.first + width.second) / dist_between_points(points[start], points[end]);
+            double percentage_change = current_ratio*100 / minimum_prev_ratio;
+            double width_change = (width.first + width.second) * 100 / (prev_width.first + prev_width.second);
+            // cout << current_ratio << " " << prev_width << '\n';
+            cout << end << "(" << points[end].first << ", " << points[end].second << ") " << percentage_change << " " << width_change << " " << percentage << '\n';
+            if(end==n-1){
+                end++;
+                prev_width = width;
+            }
+            if((percentage<percentage_change && percentage < width_change )|| end==n){
+                cout << "Ending the BB at: " << end << "(" << points[end].first << ", " << points[end].second << ")\n "<< '\n';
+                boundingBox latest;
+                latest.start = start;
+                latest.end = end-1;
+                latest.width = prev_width;
+                res.push_back(latest);
+                start = end-1;
+                difference = difference * 0.9;
+                found = true;
+                break;
+            }
+            prev_width = width;
+            minimum_prev_ratio = current_ratio;
+            end++;
+            difference = difference*0.9;
+        }
+        if(end>=n){
+            break;
+        }
+
     }
-    // if only one point is left behind, we are ignoring it
-    return curr_length;
+    return res;
 }
 
 
@@ -145,26 +144,62 @@ void points_generator()
         points.push_back(point);
     }
 }
-
+vector<pair<double, double> > read_dataset(string fname)
+{
+    vector<pair<double, double> >  points;
+    string line, word;
+    int k = 0;
+    fstream file(fname, ios::in);
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            int i = 0;
+            pair<double, double> row;
+            stringstream str(line);
+            getline(str, word, ',');
+            row.first = stof(word);
+            getline(str, word);
+            row.second = stof(word);
+            points.push_back(row);
+            k = 1;
+        }
+    }
+    else
+        cout << "Could not open the file\n";
+    return points;
+}
 int main()
 {
-    points_generator();
+    vector<pair<double, double> > points = read_dataset("Plot_2.csv");
+    // for(auto i: points){
+    //     cout << i.first << " " << i.second << '\n';
+    // }
+    int n = 20;
+    vector<boundingBox> res = bounding_box_segmentations(points, 60, 250);
+    for(auto i: res){
+        cout << "Starts at: " << i.start << '\n';
+        cout << "Ends at:   " << i.end << '\n';
+        cout << "Width is: " << i.width.first + i.width.second << '\n';
+        cout << "Length is: " << dist_between_points(points[i.start], points[i.end]) << "\n\n";
+    }
+    // points_generator();
     // for(int i=0;i<100;i++)
     // {
     //     cout<<points[i].first<<' '<<points[i].second<<'\n';
     // }
-    int segments = bounding_box_segmentations(points);
-    cout << segments << '\n';
+    // int segments = bounding_box_segmentations(points);
+    // cout << segments << '\n';
 
-    int segs = line_segments.size();
-    cout << segs << '\n';
+    // int segs = line_segments.size();
+    // cout << segs << '\n';
 
-    for(int i=0;i<segs;i++)
-    {
-        for(int j=0;j<line_segments[i].size();j++)
-        {
-            cout << line_segments[i][j] << ' ';
-        }
-        cout << '\n';
-    }
+    // for(int i=0;i<segs;i++)
+    // {
+    //     for(int j=0;j<line_segments[i].size();j++)
+    //     {
+    //         cout << line_segments[i][j] << ' ';
+    //     }
+    //     cout << '\n';
+    // }
 }
